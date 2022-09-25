4af820bbb221a9d9b7ede946b700ad2efa1148a4
Giggbit <danya19981017@gmail.com>
2022-09-26 00:34:41 +0200

    Добавьте файлы проекта.

---
+++ 21.09.2022 Шаблоны классов. Финал.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "Array.h"
+using namespace std;
+
+int main() {
+	Array<int> str(3);
+	str.Rand();
+	//str.Print(); 
+
+	Array<int> s(str);
+	//s.Print();
+	s.Add(5);
+	
+	Array<int> str2;
+	str2 = s;
+	str2.Print();
+
+}
+
